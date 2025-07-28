63694ad9da73fe55bf56f8324f40c7dd2ba507fb
meenado89 <meenakshiemail89@gmail.com>
2025-07-29 02:21:07 +0530

    donation, total, alert

--- fluffcore/src/components/Booking.jsx
+++ fluffcore/src/components/Booking.jsx
@@ -6,28 +6,57 @@ import { useRef, useEffect, useState } from "react";
 import hero4 from "../assets/heroimg4.jpg";
 
 const Booking = () => {
-  const formRef = useRef();   
+  const formRef = useRef();
   const handleSubmit = (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const form = new FormData(formRef.current);
-    const formData = {};
-    for (let [key, value] of form.entries()) {
-      formData[key] = value;
-    }
+  const form = new FormData(formRef.current);
+  const formData = {};
+  for (let [key, value] of form.entries()) {
+    formData[key] = value.trim(); 
+  }
 
-    console.log("Form submitted:", formData);
-    alert("Pre-booking successful!");
-  };
+  
+  const hasEmptyFields = Object.values(formData).some(
+    (value) => value === ""
+  );
 
-  const [quantity, setQuantity] = useState(1)
-  const plus = () => {
-  setQuantity(quantity + 1);
+  if (hasEmptyFields) {
+    alert("Please fill in all fields before submitting.");
+    return;
+  }
+
+  console.log("Form submitted:", formData);
+  alert("Pre-booking successful!");
 };
 
-const minus = () => {
-  if (quantity > 1) {
-    setQuantity(quantity - 1);
+
+  //  QUANTITY CONTROL
+  const [quantity, setQuantity] = useState(1);
+  const plus = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const minus = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+  // TOTAL PRICE
+  const unitPrice = 800;
+  // const calculateTotal = () => {
+  //   return quantity * unitPrice;
+  // };
+
+  // donation
+  const [donation, setDonation] = useState(0);
+
+  const handleDonationChange = (e) => {
+  const selectedAmount = parseInt(e.target.value);
+  setDonation(selectedAmount);
+
+  if (selectedAmount > 0) {
+    alert("THANK YOU FOR DONATION");
   }
 };
 
@@ -45,7 +74,6 @@ const minus = () => {
           <span>Reserve Your Favorite Fluff — Before They Crawl Away!</span>
         </div>
       </div>
-
       <div className="form-container">
         <div className="Cform col-12 col-md-6">
           <div className="form-container p-4">
@@ -101,42 +129,132 @@ const minus = () => {
               </div>
 
               {/* quantity counter */}
-              <div className="quantityprice-wrapper d-flex p-3">
+              <div className="quantity-price-wrapper d-flex p-3">
                 <div className="row">
                   <div className="col-6 ">
                     <div className="quantity-wrapper d-flex ">
-                     <button type="button" className="decrement" onClick={minus}>-</button>
-                    <span>{quantity}</span>
-                  
-                    <button type="button" className="increment" onClick={plus}>+</button>
+                      <button
+                        type="button"
+                        className="decrement"
+                        onClick={minus}
+                      >
+                        -
+                      </button>
+                      <span>{quantity}</span>
+
+                      <button
+                        type="button"
+                        className="increment"
+                        onClick={plus}
+                      >
+                        +
+                      </button>
                     </div>
                   </div>
                   <div className="col-6">
                     {/* updated price */}
                     <div className="total-price">
-                      Total
-                      {/* Total: ${calculateTotal()} */}
+                      Total: ₹{quantity * unitPrice + donation}
                     </div>
                   </div>
                 </div>
               </div>
-                <div className="donation-container w-75 text-center pe-5">
-                  <div className="donation-heading">
-                  <h4>
-                      Help an Real Animal Too!
-                  </h4>
+              <div className="donation-container w-75 text-center pe-5">
+                <div className="donation-heading">
+                  <h4>Help an Real Animal Too!</h4>
                   <h6>
-                    Add a donation to support animal
-                     shelters - every dollar helps a real fluff find their home!
+                    Add a donation to support animal shelters - every dollar
+                    helps a real fluff find their home!
                   </h6>
+                  <div className="donation-form">
+                    <div className="container">
+                      <div className="donation-wrapper">
+                        <div className="form-check">
+                          <div className="row">
+                            <div className="col-6 d-flex flex-column gap-2">
+                              <div>
+                                <input
+                                  type="radio"
+                                  className="form-check-input"
+                                  name="donation"
+                                  id="donation0"
+                                  value="0"
+                                  checked={donation === 0}
+                                  onChange={handleDonationChange}
+                                />
+                                <label
+                                  className="form-check-label"
+                                  htmlFor="donation0"
+                                >
+                                  ₹0
+                                </label>
+                              </div>
+
+                              <div>
+                                <input
+                                  type="radio"
+                                  className="form-check-input"
+                                  name="donation"
+                                  id="donation5"
+                                  value="5"
+                                  checked={donation === 5}
+                                  onChange={handleDonationChange}
+                                />
+                                <label
+                                  className="form-check-label"
+                                  htmlFor="donation5"
+                                >
+                                  ₹5
+                                </label>
+                              </div>
+                              <div>
+                                <input
+                                  type="radio"
+                                  className="form-check-input"
+                                  name="donation"
+                                  id="donation10"
+                                  value="10"
+                                  checked={donation === 10}
+                                  onChange={handleDonationChange}
+                                />
+                                <label
+                                  className="form-check-label"
+                                  htmlFor="donation10"
+                                >
+                                  ₹10
+                                </label>
+                              </div>
+                              <div>
+                                <input
+                                  type="radio"
+                                  className="form-check-input"
+                                  name="donation"
+                                  id="donation15"
+                                  value="15"
+                                  checked={donation === 15}
+                                  onChange={handleDonationChange}
+                                />
+                                <label
+                                  className="form-check-label"
+                                  htmlFor="donation15"
+                                >
+                                  ₹15
+                                </label>
+                              </div>
+                            </div>
+                          </div>
+                          {/* <p className="mt-3">${donation}</p> */}
+                        </div>
+                      </div>
+                    </div>
                   </div>
+
+                  <button type="submit" className="contact-btn w-25">
+                    Pre-Book Now
+                  </button>
                 </div>
-
-
-              <button type="submit" className="contact-btn w-25">
-                Pre-Book Now
-              </button>
-            </form>
+              </div>
+            </form>{" "}
           </div>
         </div>
       </div>
