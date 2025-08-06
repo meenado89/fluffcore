bd6c0aaf0e7dfa78879149f2cb1ca988f19c6061
meenado89 <meenakshiemail89@gmail.com>
2025-08-06 23:22:38 +0530

    navslider

--- fluffcore/src/components/NavBar.jsx
+++ fluffcore/src/components/NavBar.jsx
@@ -1,15 +1,30 @@
+// Navbar.jsx - Updated
 import { useState } from 'react';
-import "./Navbar.css"
+import "./Navbar.css";
+import SliderMenu from './SliderMenu';
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const closeMenu = () => {
+        setIsOpen(false);
+    };
+
     return (
         <>
-            <button className="menu-btn" onClick={() => setIsOpen(!isOpen)}>
-               <div className='Micon'>
-               
-               </div>
+            <button 
+                className="menu-btn" 
+                onClick={toggleMenu}
+                style={{ zIndex: isOpen ? 997 : 'auto' }}
+            >
+               <div className='Micon'></div>
             </button>
+            
+            <SliderMenu isOpen={isOpen} onClose={closeMenu} />
         </>
     );
 };
